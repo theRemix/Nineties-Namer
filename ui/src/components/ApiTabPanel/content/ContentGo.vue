998653a2964b0508a42b88dec432c76c3a31e65a
Jon Borgonia <jon@gomagames.com>
2017-06-21 14:55:19 -1000

    ui - fix code sample syntax and spacing

--- ui/src/components/ApiTabPanel/content/ContentGo.vue
+++ ui/src/components/ApiTabPanel/content/ContentGo.vue
@@ -2,20 +2,20 @@
   <div class="content-go">
     <div class="code">
       <span class="code-syntax-blue">import </span><span>(</span>
-      <div class="code-inner">
-        <span class="code-syntax-pink">fmt"</span>
+      <div>
+        <span class="code-syntax-pink">&nbsp;&nbsp;&quot;fmt&quot;</span>
         <br>
-        <span class="code-syntax-pink">"github.com/theRemix/go90s"</span>
+        <span class="code-syntax-pink">&nbsp;&nbsp;&quot;github.com/theRemix/go90s&quot;</span>
       </div>
       <span>)</span>
     </div>
     <div class="code">
       <span class="code-syntax-blue">func </span><span class="code-syntax-pink">main </span><span>() {</span>
-      <div class="code-inner">
-        <span>fmt.Println(<span class="code-syntax-blue">go90s.GetRandomName></span>())</span>
+      <div>
+        <span>&nbsp;&nbsp;fmt.Println(<span class="code-syntax-blue">go90s.GetRandomName</span>())</span>
         <br>
 
-        <span>fmt.Println(<span class="code-syntax-blue">go90s.GetName></span>(<span class="code-syntax-pink">v1.2.0-rc8"</span>))</span>
+        <span>&nbsp;&nbsp;fmt.Println(<span class="code-syntax-blue">go90s.GetName</span>(<span class="code-syntax-pink">&quot;v1.2.0-rc8&quot;</span>))</span>
       </div>
      <span>}</span>
     </div>
--- ui/src/components/ApiTabPanel/content/ContentJs.vue
+++ ui/src/components/ApiTabPanel/content/ContentJs.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="content-js">
     <div class="code">
-      <span><span class="code-syntax-blue">fetch</span>(<span class="code-syntax-pink"> '/random' </span>)</span>
-      <div class="code-inner">
-        <span>.<span class="code-syntax-blue">then </span>(<span class="code-syntax-pink"> res </span>=> <span class="code-syntax-pink">res</span>.text() )</span>
+      <span><span class="code-syntax-blue">fetch</span>(<span class="code-syntax-pink"> &quot;https://90s.fun/random&quot; </span>)</span>
+      <div>
+        <span>&nbsp;&nbsp;.<span class="code-syntax-blue">then</span>(<span class="code-syntax-pink"> res </span>=> <span class="code-syntax-pink">res</span>.text() )</span>
         <br>
-        <span>.<span class="code-syntax-blue">then </span>(<span class="code-syntax-pink"> randomName</span> => {</span>
-        <div class="code-inner">
-          <span><span class="code-syntax-blue">console</span>.<span class="code-syntax-blue">log</span>( <span class="code-syntax-pink">randomName  </span>);</span>
+        <span>&nbsp;&nbsp;.<span class="code-syntax-blue">then</span>(<span class="code-syntax-pink"> randomName</span> => {</span>
+        <div>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="code-syntax-blue">console</span>.<span class="code-syntax-blue">log</span>( <span class="code-syntax-pink">randomName</span> )</span>
         </div>
-        <span>})</span>
+        <span>&nbsp;&nbsp;})</span>
       </div>
     </div>
-    
+
     <br>
-    <span><span class="code-syntax-blue">const someKey </span>= <span class="code-syntax-pink">'v1.2.0-rc8'</span>;</span>
+    <span><span class="code-syntax-blue">const someKey </span>= <span class="code-syntax-pink">&quot;v1.2.0-rc8&quot;</span>;</span>
     <div class="code">
-      <span><span class="code-syntax-blue">fetch</span>(<span class="code-syntax-pink">`/names/</span>${<span class="code-syntax-blue">someKey</span>}<span class="code-syntax-pink">`</span>)</span>
+      <span><span class="code-syntax-blue">fetch</span>( <span class="code-syntax-pink">&#96;https://90s.fun/names/</span>${<span class="code-syntax-blue">someKey</span>}<span class="code-syntax-pink">&#96;</span> )</span>
       <br>
-      <div class="code-inner">
-        <span>.<span class="code-syntax-blue">then</span>( <span class="code-syntax-pink">res</span> => <span class="code-syntax-pink">res</span>.text() )</span>
+      <div>
+        <span>&nbsp;&nbsp;.<span class="code-syntax-blue">then</span>( <span class="code-syntax-pink">res</span> => <span class="code-syntax-pink">res</span>.text() )</span>
         <br>
-        <span>.<span class="code-syntax-blue">then</span>( <span class="code-syntax-pink">generatedName</span> => {</span>
-        <div class="code-inner">
-          <span><span class="code-syntax-blue">console</span>.<span class="code-syntax-blue">log</span>(<span class="code-syntax-pink"> generatedName</span> );</span>
+        <span>&nbsp;&nbsp;.<span class="code-syntax-blue">then</span>( <span class="code-syntax-pink">generatedName</span> => {</span>
+        <div>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="code-syntax-blue">console</span>.<span class="code-syntax-blue">log</span>(<span class="code-syntax-pink"> generatedName</span> )</span>
         </div>
-        <span>})</span>
+        <span>&nbsp;&nbsp;})</span>
       </div>
     </div>
   </div>
